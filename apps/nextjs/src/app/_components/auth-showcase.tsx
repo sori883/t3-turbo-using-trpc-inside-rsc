e7d5011321c34f77c8ba241794ced3d9c87184fa
sorinaji <37400002+sori883@users.noreply.github.com>
2023-10-26 20:15:46 +0900

    discord -> google
    discordのキーない

--- apps/nextjs/src/app/_components/auth-showcase.tsx
+++ apps/nextjs/src/app/_components/auth-showcase.tsx
@@ -8,10 +8,10 @@ export async function AuthShowcase() {
   if (!session) {
     return (
       <SignIn
-        provider="discord"
+        provider="google"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
       >
-        Sign in with Discord
+        Sign in with Google
       </SignIn>
     );
   }
--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,4 +1,4 @@
-import Discord from "@auth/core/providers/discord";
+import Google from "@auth/core/providers/google";
 import type { DefaultSession } from "@auth/core/types";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
@@ -10,7 +10,7 @@ import { env } from "./env.mjs";
 export type { Session } from "next-auth";
 
 // Update this whenever adding new providers so that the client can
-export const providers = ["discord"] as const;
+export const providers = ["google"] as const;
 export type OAuthProviders = (typeof providers)[number];
 
 declare module "next-auth" {
@@ -28,9 +28,9 @@ export const {
 } = NextAuth({
   adapter: DrizzleAdapter(db, tableCreator),
   providers: [
-    Discord({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
